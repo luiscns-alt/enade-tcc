df83525c8987e21265d53696840fbed677fa7c3e
Luis Carlos <luiscarlosnors@gmail.com>
2023-10-10 21:31:39 -0300

    feat: Add logs in response API

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,18 @@
+import { LogLevel } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { LoggerInterceptor } from './utils/logging.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const isProduction = process.env.NODE_ENV === 'production';
+  const logLevels: LogLevel[] = isProduction
+    ? ['error', 'warn', 'log']
+    : ['error', 'warn', 'log', 'debug', 'verbose'];
+
+  const app = await NestFactory.create(AppModule, {
+    logger: logLevels,
+  });
   app.enableCors();
 
   const config = new DocumentBuilder()
@@ -15,6 +24,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  app.useGlobalInterceptors(new LoggerInterceptor());
+
+  await app.listen(process.env.PORT || 3000);
 }
-bootstrap();
+bootstrap().then(() =>
+  console.log(`Application is running on: ${process.env.PORT || 3000}`),
+);
--- server/src/prisma.service.ts
+++ server/src/prisma.service.ts
@@ -1,10 +1,56 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import {
+  INestApplication,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient<Prisma.PrismaClientOptions, Prisma.LogLevel>
+  implements OnModuleInit
+{
+  private readonly logger = new Logger(PrismaService.name);
+
+  constructor() {
+    super({
+      log: [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+        {
+          emit: 'event',
+          level: 'error',
+        },
+        {
+          emit: 'event',
+          level: 'info',
+        },
+        {
+          emit: 'event',
+          level: 'warn',
+        },
+      ],
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
+
+    this.$on('error', ({ message }) => {
+      this.logger.error(message);
+    });
+    this.$on('warn', ({ message }) => {
+      this.logger.warn(message);
+    });
+    this.$on('info', ({ message }) => {
+      this.logger.debug(message);
+    });
+    this.$on('query', ({ query, params }) => {
+      this.logger.log(`${query}; ${params}`);
+    });
   }
 
   async enableShutdownHooks(app: INestApplication) {
